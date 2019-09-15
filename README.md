2d53aaa295dd2010d2d11ab6dc56cd5f4c56a584
Heyward Fann <fannheyward@gmail.com>
2019-09-15 10:12:40 +0800

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# coc-terminal
+
+Toggle terminal with coc.nvim. Inspired by [repl with coc.nvim](https://gist.github.com/chemzqm/e22ddf489ec24c7d21bbcb047f4b3f86).
+
+## Install
+
+`:CocInstall coc-terminal`
+
+## Keymaps
+
+`<Plug>(coc-terminal-toggle)`: toggle terminal show/hide
+
+## Commands
+
+- `terminal.Toggle`: Show/hide terminal
+- `terminal.Destroy`: Destroy and free terminal
+
+## License
+
+MIT
+
+---
+> This extension is created by [create-coc-extension](https://github.com/fannheyward/create-coc-extension)
---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import { Terminal, commands, ExtensionContext, workspace } from 'coc.nvim';
+
+let terminal: Terminal | undefined;
+let showing: boolean = false;
+
+export async function activate(context: ExtensionContext): Promise<void> {
+  workspace.showMessage(`coc-terminal works!`);
+
+  context.subscriptions.push(
+    commands.registerCommand('terminal.Toggle', async () => {
+      await toggle();
+    }),
+
+    commands.registerCommand('terminal.Destroy', () => {
+      if (terminal) {
+        terminal.dispose();
+        terminal = undefined;
+      }
+    }),
+
+    workspace.registerKeymap(
+      ['n'],
+      'terminal-toggle',
+      async () => {
+        await toggle();
+      },
+      { sync: false }
+    )
+  );
+}
+
+async function toggle(): Promise<void> {
+  if (!terminal) {
+    terminal = await workspace.createTerminal({ name: 'coc-terminal' });
+    if (!terminal) {
+      workspace.showMessage(`Create terminal failed`, 'error');
+      return;
+    }
+
+    terminal.show();
+    showing = true;
+    return;
+  }
+
+  if (showing) {
+    terminal.hide();
+    showing = false;
+  } else {
+    terminal.show();
+    showing = true;
+  }
+}
