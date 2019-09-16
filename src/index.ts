c7722cb11d40930444d26600a30d96639dc1f215
Heyward Fann <fannheyward@gmail.com>
2019-09-16 16:32:25 +0800

    feat(extension): REPL support

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Terminal, commands, ExtensionContext, workspace } from 'coc.nvim';
 
+const replAvailableLanguages = ['javascript', 'typescript', 'python'];
 let terminal: Terminal | undefined;
 let showing: boolean = false;
 
@@ -7,6 +8,10 @@ export async function activate(context: ExtensionContext): Promise<void> {
   context.subscriptions.push(
     commands.registerCommand('terminal.Toggle', async () => {
       await toggle();
+    }),
+
+    commands.registerCommand('terminal.REPL', async () => {
+      await repl();
     }),
 
     commands.registerCommand('terminal.Destroy', () => {
@@ -29,10 +34,11 @@ export async function activate(context: ExtensionContext): Promise<void> {
 
 async function toggle(): Promise<void> {
   if (terminal) {
-    const doc = await workspace.getDocument(terminal.bufnr);
+    const doc = workspace.getDocument(terminal.bufnr);
     if (doc) {
       workspace.jumpTo(doc.uri);
     } else {
+      terminal.dispose();
       terminal = undefined;
     }
   }
@@ -55,4 +61,35 @@ async function toggle(): Promise<void> {
     terminal.show();
     showing = true;
   }
+}
+
+async function repl(): Promise<void> {
+  const { nvim } = workspace;
+  const filetype = await nvim.eval('&filetype');
+  if (!replAvailableLanguages.includes(filetype.toString())) {
+    workspace.showMessage(`No REPL support for ${filetype} yet`, 'warning');
+    return;
+  }
+
+  let prog = '';
+  if (filetype === 'javascript') {
+    prog = 'node';
+  } else if (filetype === 'typescript') {
+    prog = 'ts-node';
+  } else if (filetype === 'python') {
+    prog = 'python';
+  }
+
+  if (terminal) {
+    terminal.dispose();
+  }
+  terminal = await workspace.createTerminal({ name: 'coc-terminal' });
+  if (!terminal) {
+    workspace.showMessage(`Create terminal failed`, 'error');
+    return;
+  }
+
+  terminal.sendText(prog, true);
+  await nvim.command('startinsert');
+  showing = true;
 }
